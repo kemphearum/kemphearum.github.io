efe558743b92764531a69ab84505e954fa9e6bad
phearum <kem.phearum@gmail.com>
2026-02-18 17:41:06 +0700

    Update deploy config for GitHub Pages root domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // IMPORTANT: Replace 'portfolio' with your GitHub repository name
+  base: '/',
 })
