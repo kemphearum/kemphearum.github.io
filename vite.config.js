10e37369f786d8f3543f4b35178c2f9328ef1bac
kemphearum <kem.phearum@gmail.com>
2026-03-14 10:59:18 +0700

    fix: add @vercel/react-router preset to vite config to fix 500 error on vercel deployments

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter({ presets: [vercelPreset()] })],
   test: {
     globals: true,
     environment: 'jsdom',
